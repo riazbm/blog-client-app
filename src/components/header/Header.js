3bd7eac486f6f8118cd20806216af7f66aeeeff4
riziki <rizikimohamed968@gmail.com>
2023-11-14 18:20:55 +0300

    menu folder created and imported in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Logo from "./mini_component/Logo";
+import Menu from "./mini_component/Menu";
 
 
 const Header = () =>{
     return(
         <header className="header">
-        <nav className="header-nav"></nav>
+        <nav className="header-nav">
             <Logo/>
+            <Menu/>
+        </nav>
         </header>
     )
 }
---
+++ src/components/header/mini_component/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Menu = () =>{
+    return(
+        <nav>
+            <Menu/>
+        </nav>
+    )
+}
+export default Menu

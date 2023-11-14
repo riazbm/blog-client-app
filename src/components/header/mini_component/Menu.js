6b7d577677c3c4dfed5cbe263112e93d0d8d993a
riziki <rizikimohamed968@gmail.com>
2023-11-14 20:19:51 +0300

    hooks start

--- src/components/header/mini_component/Menu.js
+++ src/components/header/mini_component/Menu.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import ToggleButton from "./ToggleButton";
 import Dropdown from "./Dropdown";
 
 
 const Menu = () =>{
+    const [show, setshow]  = useState(false)
     return(
         <nav className="menu">
             

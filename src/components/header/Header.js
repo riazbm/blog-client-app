8bc3b85494de287f43fac43269cb23d267f9e937
riziki <rizikimohamed968@gmail.com>
2023-11-14 17:39:31 +0300

    padding

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ import Logo from "./mini_component/Logo";
 const Header = () =>{
     return(
         <header className="header">
-        <nav className="headeer-nav"></nav>
+        <nav className="header-nav"></nav>
             <Logo/>
         </header>
     )
